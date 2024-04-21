64c8b050b8d9c289cc94837809ad9fa1a198e3ca
ppericas <peppericasserra02@gmail.com>
2024-04-21 16:55:28 +0200

    name stage arreglada

--- assets.py
+++ assets.py
@@ -1,11 +1,15 @@
 # ----------------------------------------------------------------------------------------------------------------------
-import pygame
+import pygame, os
 pygame.init()
-import os
 # ----------------------------------------------------------------------------------------------------------------------
 # sizes
 WIDTH: int = 600
 HEIGHT: int = WIDTH/2
+
+CARD_HEIGHT: int = 129
+CARD_WIDTH: int = 107
+CARD_X: int = 164
+CARD_Y: int = 68
 
 # ----------------------------------------------------------------------------------------------------------------------
 # colors
@@ -17,20 +21,8 @@ DARK_GREY: tuple[int] = (66, 66, 66)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # images
-PLAY_STAGE_IMAGE = pygame.image.load(
-    os.path.join('assets', 'play_stage.png'))
-PLAY_STAGE = pygame.transform.scale(PLAY_STAGE_IMAGE, (WIDTH, HEIGHT))
-
-NAME_STAGE_IMAGE = pygame.image.load(
-    os.path.join('assets', 'name_stage.png'))
-NAME_STAGE= pygame.transform.scale(NAME_STAGE_IMAGE, (WIDTH, HEIGHT))
-
-TUTORIAL_STAGE_IMAGE = pygame.image.load(
-    os.path.join('assets', 'tutorial_stage.png'))
-TUTORIAL_STAGE = pygame.transform.scale(TUTORIAL_STAGE_IMAGE, (WIDTH, HEIGHT))
 
 # baraja = []
-
 # suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 # ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
@@ -38,8 +30,6 @@ TUTORIAL_STAGE = pygame.transform.scale(TUTORIAL_STAGE_IMAGE, (WIDTH, HEIGHT))
 #     for rank in ranks:
 #         card_name = f"card{suit}{rank}.png"
 #         baraja.append(card_name)
-
-# print(baraja)
 
 # Lista de nombres de las imágenes de las cartas
 # cards = ['cardClubsA.png', 'cardClubs2.png', 'cardClubs3.png', 'cardClubs4.png', 'cardClubs5.png', 'cardClubs6.png',
@@ -64,122 +54,232 @@ TUTORIAL_STAGE = pygame.transform.scale(TUTORIAL_STAGE_IMAGE, (WIDTH, HEIGHT))
 #     os.path.join('assets', '{nombre_imagen_en_minusculas}.png'))
 # {NOMBRE_IMAGEN_EN_MAYUSCULAS} = pygame.transform.scale({NOMBRE_IMAGEN_EN_MAYUSCULAS}_IMAGE, (WIDTH, HEIGHT))
 
-# ['cardClubsA.png', 'cardClubs2.png', 'cardClubs3.png', 'cardClubs4.png', 'cardClubs5.png', 'cardClubs6.png', 'cardClubs7.png', 'cardClubs8.png', 'cardClubs9.png', 'cardClubs10.png', 
-# 'cardClubsJ.png', 'cardClubsQ.png', 'cardClubsK.png', 'cardDiamondsA.png', 'cardDiamonds2.png', 'cardDiamonds3.png', 'cardDiamonds4.png', 'cardDiamonds5.png', 'cardDiamonds6.png', 'cardDiamonds7.png', 'cardDiamonds8.png', 'cardDiamonds9.png', 'cardDiamonds10.png', 'cardDiamondsJ.png', 'cardDiamondsQ.png', 'cardDiamondsK.png', 'cardHeartsA.png', 'cardHearts2.png', 'cardHearts3.png', 'cardHearts4.png', 'cardHearts5.png', 'cardHearts6.png', 'cardHearts7.png', 'cardHearts8.png', 'cardHearts9.png', 'cardHearts10.png', 'cardHeartsJ.png', 'cardHeartsQ.png', 'cardHeartsK.png', 'cardSpadesA.png', 'cardSpades2.png', 'cardSpades3.png', 'cardSpades4.png', 'cardSpades5.png', 'cardSpades6.png', 'cardSpades7.png', 'cardSpades8.png', 
-# 'cardSpades9.png', 'cardSpades10.png', 'cardSpadesJ.png', 'cardSpadesQ.png', 'cardSpadesK.png']
+PLAY_STAGE_IMAGE = pygame.image.load(
+    os.path.join('assets', 'play_stage.png'))
+PLAY_STAGE = pygame.transform.scale(PLAY_STAGE_IMAGE, (WIDTH, HEIGHT))
 
-CARD_HEIGHT, CARD_WIDTH = 129, 107
-CARD_X, CARD_Y = 164, 68
+NAME_STAGE_IMAGE = pygame.image.load(
+    os.path.join('assets', 'name_stage.png'))
+NAME_STAGE= pygame.transform.scale(NAME_STAGE_IMAGE, (WIDTH, HEIGHT))
 
-#cardBack_red4
+TUTORIAL_STAGE_IMAGE = pygame.image.load(
+    os.path.join('assets', 'tutorial_stage.png'))
+TUTORIAL_STAGE = pygame.transform.scale(TUTORIAL_STAGE_IMAGE, (WIDTH, HEIGHT))
 
-CARDBACK_IMAGE = pygame.image.load(os.path.join('assets', 'cardback.png'))
+CARDBACK_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardback.png'))
 CARDBACK = pygame.transform.scale(CARDBACK_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBSA_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubsa.png'))
+
+CARDCLUBSA_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubsa.png'))
 CARDCLUBSA = pygame.transform.scale(CARDCLUBSA_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS2_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs2.png'))
+
+CARDCLUBS2_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs2.png'))
 CARDCLUBS2 = pygame.transform.scale(CARDCLUBS2_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS3_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs3.png'))
+
+CARDCLUBS3_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs3.png'))
 CARDCLUBS3 = pygame.transform.scale(CARDCLUBS3_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS4_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs4.png'))
+
+CARDCLUBS4_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs4.png'))
 CARDCLUBS4 = pygame.transform.scale(CARDCLUBS4_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS5_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs5.png'))
+
+CARDCLUBS5_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs5.png'))
 CARDCLUBS5 = pygame.transform.scale(CARDCLUBS5_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS6_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs6.png'))
+
+CARDCLUBS6_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs6.png'))
 CARDCLUBS6 = pygame.transform.scale(CARDCLUBS6_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS7_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs7.png'))
+
+CARDCLUBS7_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs7.png'))
 CARDCLUBS7 = pygame.transform.scale(CARDCLUBS7_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS8_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs8.png'))
+
+CARDCLUBS8_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs8.png'))
 CARDCLUBS8 = pygame.transform.scale(CARDCLUBS8_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS9_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs9.png'))
+
+CARDCLUBS9_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs9.png'))
 CARDCLUBS9 = pygame.transform.scale(CARDCLUBS9_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBS10_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubs10.png'))
+
+CARDCLUBS10_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubs10.png'))
 CARDCLUBS10 = pygame.transform.scale(CARDCLUBS10_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBSJ_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubsj.png'))
+
+CARDCLUBSJ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubsj.png'))
 CARDCLUBSJ = pygame.transform.scale(CARDCLUBSJ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBSQ_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubsq.png'))
+
+CARDCLUBSQ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubsq.png'))
 CARDCLUBSQ = pygame.transform.scale(CARDCLUBSQ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDCLUBSK_IMAGE = pygame.image.load(os.path.join('assets', 'cardclubsk.png'))
+
+CARDCLUBSK_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardclubsk.png'))
 CARDCLUBSK = pygame.transform.scale(CARDCLUBSK_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDSA_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamondsa.png'))
+
+CARDDIAMONDSA_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamondsa.png'))
 CARDDIAMONDSA = pygame.transform.scale(CARDDIAMONDSA_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS2_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds2.png'))
+
+CARDDIAMONDS2_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds2.png'))
 CARDDIAMONDS2 = pygame.transform.scale(CARDDIAMONDS2_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS3_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds3.png'))
+
+CARDDIAMONDS3_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds3.png'))
 CARDDIAMONDS3 = pygame.transform.scale(CARDDIAMONDS3_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS4_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds4.png'))
+
+CARDDIAMONDS4_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds4.png'))
 CARDDIAMONDS4 = pygame.transform.scale(CARDDIAMONDS4_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS5_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds5.png'))
+
+CARDDIAMONDS5_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds5.png'))
 CARDDIAMONDS5 = pygame.transform.scale(CARDDIAMONDS5_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS6_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds6.png'))
+
+CARDDIAMONDS6_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds6.png'))
 CARDDIAMONDS6 = pygame.transform.scale(CARDDIAMONDS6_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS7_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds7.png'))
+
+CARDDIAMONDS7_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds7.png'))
 CARDDIAMONDS7 = pygame.transform.scale(CARDDIAMONDS7_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS8_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds8.png'))
+
+CARDDIAMONDS8_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds8.png'))
 CARDDIAMONDS8 = pygame.transform.scale(CARDDIAMONDS8_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS9_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds9.png'))
+
+CARDDIAMONDS9_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds9.png'))
 CARDDIAMONDS9 = pygame.transform.scale(CARDDIAMONDS9_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDS10_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamonds10.png'))
+
+CARDDIAMONDS10_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamonds10.png'))
 CARDDIAMONDS10 = pygame.transform.scale(CARDDIAMONDS10_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDSJ_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamondsj.png'))
+
+CARDDIAMONDSJ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamondsj.png'))
 CARDDIAMONDSJ = pygame.transform.scale(CARDDIAMONDSJ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDSQ_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamondsq.png'))
+
+CARDDIAMONDSQ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamondsq.png'))
 CARDDIAMONDSQ = pygame.transform.scale(CARDDIAMONDSQ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDDIAMONDSK_IMAGE = pygame.image.load(os.path.join('assets', 'carddiamondsk.png'))
+
+CARDDIAMONDSK_IMAGE = pygame.image.load(
+    os.path.join('assets', 'carddiamondsk.png'))
 CARDDIAMONDSK = pygame.transform.scale(CARDDIAMONDSK_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTSA_IMAGE = pygame.image.load(os.path.join('assets', 'cardheartsa.png'))
+
+CARDHEARTSA_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardheartsa.png'))
 CARDHEARTSA = pygame.transform.scale(CARDHEARTSA_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS2_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts2.png'))
+
+CARDHEARTS2_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts2.png'))
 CARDHEARTS2 = pygame.transform.scale(CARDHEARTS2_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS3_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts3.png'))
+
+CARDHEARTS3_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts3.png'))
 CARDHEARTS3 = pygame.transform.scale(CARDHEARTS3_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS4_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts4.png'))
+
+CARDHEARTS4_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts4.png'))
 CARDHEARTS4 = pygame.transform.scale(CARDHEARTS4_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS5_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts5.png'))
+
+CARDHEARTS5_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts5.png'))
 CARDHEARTS5 = pygame.transform.scale(CARDHEARTS5_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS6_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts6.png'))
+
+CARDHEARTS6_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts6.png'))
 CARDHEARTS6 = pygame.transform.scale(CARDHEARTS6_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS7_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts7.png'))
+
+CARDHEARTS7_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts7.png'))
 CARDHEARTS7 = pygame.transform.scale(CARDHEARTS7_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS8_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts8.png'))
+
+CARDHEARTS8_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts8.png'))
 CARDHEARTS8 = pygame.transform.scale(CARDHEARTS8_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS9_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts9.png'))
+
+CARDHEARTS9_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts9.png'))
 CARDHEARTS9 = pygame.transform.scale(CARDHEARTS9_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTS10_IMAGE = pygame.image.load(os.path.join('assets', 'cardhearts10.png'))
+
+CARDHEARTS10_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardhearts10.png'))
 CARDHEARTS10 = pygame.transform.scale(CARDHEARTS10_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTSJ_IMAGE = pygame.image.load(os.path.join('assets', 'cardheartsj.png'))
+
+CARDHEARTSJ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardheartsj.png'))
 CARDHEARTSJ = pygame.transform.scale(CARDHEARTSJ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTSQ_IMAGE = pygame.image.load(os.path.join('assets', 'cardheartsq.png'))
+
+CARDHEARTSQ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardheartsq.png'))
 CARDHEARTSQ = pygame.transform.scale(CARDHEARTSQ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDHEARTSK_IMAGE = pygame.image.load(os.path.join('assets', 'cardheartsk.png'))
+
+CARDHEARTSK_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardheartsk.png'))
 CARDHEARTSK = pygame.transform.scale(CARDHEARTSK_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADESA_IMAGE = pygame.image.load(os.path.join('assets', 'cardspadesa.png'))
+
+CARDSPADESA_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspadesa.png'))
 CARDSPADESA = pygame.transform.scale(CARDSPADESA_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES2_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades2.png'))
+
+CARDSPADES2_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades2.png'))
 CARDSPADES2 = pygame.transform.scale(CARDSPADES2_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES3_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades3.png'))
+
+CARDSPADES3_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades3.png'))
 CARDSPADES3 = pygame.transform.scale(CARDSPADES3_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES4_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades4.png'))
+
+CARDSPADES4_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades4.png'))
 CARDSPADES4 = pygame.transform.scale(CARDSPADES4_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES5_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades5.png'))
+
+CARDSPADES5_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades5.png'))
 CARDSPADES5 = pygame.transform.scale(CARDSPADES5_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES6_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades6.png'))
+
+CARDSPADES6_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades6.png'))
 CARDSPADES6 = pygame.transform.scale(CARDSPADES6_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES7_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades7.png'))
+
+CARDSPADES7_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades7.png'))
 CARDSPADES7 = pygame.transform.scale(CARDSPADES7_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES8_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades8.png'))
+
+CARDSPADES8_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades8.png'))
 CARDSPADES8 = pygame.transform.scale(CARDSPADES8_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES9_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades9.png'))
+
+CARDSPADES9_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades9.png'))
 CARDSPADES9 = pygame.transform.scale(CARDSPADES9_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADES10_IMAGE = pygame.image.load(os.path.join('assets', 'cardspades10.png'))
+
+CARDSPADES10_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspades10.png'))
 CARDSPADES10 = pygame.transform.scale(CARDSPADES10_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADESJ_IMAGE = pygame.image.load(os.path.join('assets', 'cardspadesj.png'))
+
+CARDSPADESJ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspadesj.png'))
 CARDSPADESJ = pygame.transform.scale(CARDSPADESJ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADESQ_IMAGE = pygame.image.load(os.path.join('assets', 'cardspadesq.png'))
+
+CARDSPADESQ_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspadesq.png'))
 CARDSPADESQ = pygame.transform.scale(CARDSPADESQ_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
-CARDSPADESK_IMAGE = pygame.image.load(os.path.join('assets', 'cardspadesk.png'))
+
+CARDSPADESK_IMAGE = pygame.image.load(
+    os.path.join('assets', 'cardspadesk.png'))
 CARDSPADESK = pygame.transform.scale(CARDSPADESK_IMAGE, (CARD_WIDTH, CARD_HEIGHT))
 
+# ----------------------------------------------------------------------------------------------------------------------
+
 # others
-FPS = 60
+FPS: int = 60
 
--- main.py
+++ main.py
@@ -1,94 +1,100 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # imports
-import pygame
-pygame.init()
-import assets, json
+import pygame, assets, json
 from icecream import ic
 pygame.display.set_caption('HoL')
+pygame.init()
 
 WIN = pygame.display.set_mode((assets.WIDTH, assets.HEIGHT))
 font = pygame.font.Font(None, 36)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # classes and functions
+def draw_window(stage: str, text_name: str, seconds: int) -> None: # TODO implementació per usar els segons
+    WIN.blit(stage, (0, 0))
+    render_images()
 
-# FIXME crec que no es necessaria amb la darrera implementació
-# def render_texts(stage: str, event) -> None:
-#     if stage == assets.NAME_STAGE:
-#         # text_surface = font.render("text", True, assets.BLACK)
-#         # WIN.blit(text_surface, (0, 0))
-#         handle_name(event)
+    if stage == assets.NAME_STAGE:
+        render_text(f"Escribe tu nickname:", assets.BLACK, ((assets.WIDTH) //3, (assets.HEIGHT) //3))
+        render_text(f"{text_name}", assets.BLACK, ((assets.WIDTH) //3, (assets.HEIGHT + 100) //3))
 
-#     elif stage == assets.TUTORIAL_STAGE:
-#         ...
+    elif stage == assets.TUTORIAL_STAGE:
 
-#     elif stage == assets.PLAY_STAGE:
-#         ...
+        render_text(f"", assets.BLACK, (0, 0))
 
 
+    elif stage == assets.PLAY_STAGE:
+        render_text(f"{seconds} s", assets.BLACK, (0, 100))
 
-
-def draw_window(stage: str, seconds: int) -> None:
-    render_images(stage)
-    # render_texts(stage)
     pygame.display.update()
 
 
-def render_images(stage) -> None:
-    WIN.blit(stage, (0, 0))
-    WIN.blit(assets.CARDCLUBS6, (assets.CARD_X, assets.CARD_Y))
+def render_text(text: str, color: tuple[int], z: tuple[int]) -> None:
+    text_surface = font.render(text, True, color)
+    WIN.blit(text_surface, z)
 
 
-def handle_click(stage) -> None:
-    new_stage = stage
+
+def render_images() -> None:
+    # WIN.blit(assets.CARDCLUBS6, (assets.CARD_X, assets.CARD_Y))
+    ...
+
+
+# This method checks in which state of the game you are, in the name stage, in the tutorial stage or in the game stage; so it doesn't have to execute the ones that doesnt interest you
+def update_stage(stage: str, text_name: str) -> str:
+    new_stage: str = stage
+    new_text_name: str = text_name
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+        if stage == assets.NAME_STAGE:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return new_stage, new_text_name
 
-        elif event.type == pygame.KEYDOWN:
-            if stage == assets.NAME_STAGE:
-                handle_name(event)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    new_stage: str = assets.TUTORIAL_STAGE
+                    return new_stage, new_text_name
 
-            elif stage == assets.TUTORIAL_STAGE:
-                # handle_tutorial(event)
-                for key in range(pygame.K_a, pygame.K_z + 1):
-                    if event.key == key:
-                        new_stage: str = assets.PLAY_STAGE
+                elif event.key == pygame.K_BACKSPACE:
+                    new_text_name: str = new_text_name[:-1]
 
-            elif stage == assets.PLAY_STAGE:
-                ...
+                else:
+                    for key in range(pygame.K_a, pygame.K_z + 1):
+                        if event.key == key:
+                            key_name = pygame.key.name(key)
+                            new_text_name += key_name
 
-    return new_stage
+        elif stage == assets.TUTORIAL_STAGE:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    new_stage: str = assets.PLAY_STAGE
+                    return new_stage, new_text_name
 
-'''
-TODO funcio per detectar qualsevol lletra del teclat
-elif event.type == pygame.KEYDOWN:
+    return new_stage, new_text_name
+
+
+def update_name(stage: str, event, text_name: str) -> str:
+    new_text_name: str = text_name
+    new_stage: str = stage
+
+    if stage == assets.NAME_STAGE:
+        if event.type == pygame.KEYDOWN: #FIXME no entra
+            if event.key == pygame.K_BACKSPACE:
+                new_text_name: str = new_text_name[:-1]
+                return new_text_name
+
+            else:
                 for key in range(pygame.K_a, pygame.K_z + 1):
                     if event.key == key:
                         key_name = pygame.key.name(key)
-                        print(f"La tecla '{key_name}' fue presionada")
-'''
-def handle_name(event) -> None:
-    name_input = "hola" 
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RETURN:
-            return name_input
+                        new_text_name += key_name
 
-        elif event.key == pygame.K_BACKSPACE:
-            name_input = name_input[:-1] # Borra el ultimo character que se ha escrito
+    return new_text_name, new_stage
 
-        else:
-            name_input += event.unicode
-
-
-#FIXME no actualitza el valor de stage
-def handle_tutorial(event):
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RETURN:
-            stage: str = assets.PLAY_STAGE
 
 def save_user_data(name: str, time_taken: int) -> None: # TODO usar aquesta funció
     top_scores: list[dict[str, int]] = []
+
     with open('top_scores.json', 'r') as file:
         top_scores = json.load(file)
 
@@ -98,19 +104,25 @@ def save_user_data(name: str, time_taken: int) -> None: # TODO usar aquesta func
     with open('top_scores.json', 'w') as file:
         json.dump(top_scores[:3], file) # save only the 5 names first from top_scores
 
+
 ## main loop
 def main() -> None:
     clock = pygame.time.Clock()
-    start_time: int = pygame.time.get_ticks()
-    run = True
-    stage: str = assets.TUTORIAL_STAGE
+    start_time: float = pygame.time.get_ticks()
+    run: bool = True
+    stage: int = assets.NAME_STAGE
+    text_name: str = ""
 
     while run:
         current_time: int = pygame.time.get_ticks()
-        chronometer: int = (current_time - start_time) / 1000
+        seconds: float = (current_time - start_time) / 1000
+        seconds: str = "{:.0f}".format(seconds) # formatea seconds a no tener decimales
         clock.tick(assets.FPS)
-        stage = handle_click(stage)
-        draw_window(stage, chronometer)
+
+        stage, text_name = update_stage(stage, text_name)
+
+        draw_window(stage, text_name, seconds)
+
     pygame.quit()
 
 if __name__ == '__main__':
